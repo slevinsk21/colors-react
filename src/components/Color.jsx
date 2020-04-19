48a787be63344c044a61dd48e60c02e14d2c7877
Leonel Castellano <slevinsk21@gmail.com>
2020-04-19 07:15:00 -0400

    overlay message

--- src/components/Color.jsx
+++ src/components/Color.jsx
@@ -9,7 +9,10 @@ import {
   CardHeader,
   CardContent,
   CardActions,
-  Divider
+  Divider,
+  Backdrop,
+  Box,
+  Chip
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -28,18 +31,47 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'flex-end'
+  },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff'
   }
 }));
 
 const Color = ({ name, color, pantone_value, year, copy }) => {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleToggle = () => {
+    setOpen(true);
+    copy();
+    setTimeout(() => {
+      setOpen(false);
+    }, 1000);
+  };
 
   return (
     <Grid item xs={12} sm={6} md={4}>
+      <Backdrop
+        className={classes.backdrop}
+        open={open}
+        style={{ backgroundColor: 'rgba(0, 0, 0, .9)' }}
+      >
+        <Typography className={classes.title} component="span">
+          <Box fontWeight="fontWeightBold" fontSize="h5.fontSize">
+            The color{' '}
+            <span style={{ textTransform: 'uppercase', color: color }}>
+              {name}
+            </span>{' '}
+            with code: {color} was copied !!!
+          </Box>
+        </Typography>
+      </Backdrop>
+
       <Card
         className={classes.card}
         style={{ backgroundColor: color }}
-        onClick={copy}
+        onClick={handleToggle}
       >
         <CardHeader title={year} />
         <CardContent className={classes.cardContent}>
